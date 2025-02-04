78e7c06d182b2b0e4ae72c0ba8e47cfeb5f76a12
Egor <egorr.str@gmail.com>
2025-02-04 21:22:21 +1000

    complex numbers project

--- src/complex_n.py
+++ src/complex_n.py
@@ -76,6 +76,9 @@ class Rational:
     def __add__(self, other):
         """
         Сложение двух рациональных чисел.
+
+        :param other: Другое рациональное число или число типа int/float.
+        :return: Результат сложения.
         """
         if isinstance(other, Rational):
             new_numerator = self.__numerator * other.__denominator + other.__numerator * self.__denominator
@@ -88,6 +91,8 @@ class Rational:
     def __sub__(self, other):
         """
         Вычитание двух рациональных чисел.
+        :param other: Другое рациональное число или число типа int/float.
+        :return: Результат вычитания.
         """
         if isinstance(other, Rational):
             new_numerator = self.__numerator * other.__denominator - other.__numerator * self.__denominator
@@ -100,6 +105,8 @@ class Rational:
     def __mul__(self, other):
         """
         Умножение двух рациональных чисел.
+        :param other: Другое рациональное число или число типа int/float.
+        :return: Результат умножения.
         """
         if isinstance(other, Rational):
             new_numerator = self.__numerator * other.__numerator
@@ -112,6 +119,9 @@ class Rational:
     def __truediv__(self, other):
         """
         Деление двух рациональных чисел.
+        :param other: Другое рациональное число или число типа int/float.
+        :return: Результат деления.
+        :raises ZeroDivisionError: Если делитель равен нулю.
         """
         if isinstance(other, Rational):
             if other.__numerator == 0:
@@ -128,12 +138,15 @@ class Rational:
     def __neg__(self):
         """
         Возвращает противоположное рациональное число.
+        :return: Противоположное рациональное число.
         """
         return Rational(-self.__numerator, self.__denominator)
 
     def __eq__(self, other):
         """
-        Проверка равенства двух рациональных чисел.
+        Проверяет равенство текущего рационального числа с другим числом.
+        :param other: Другое рациональное число или число типа int/float.
+        :return: True, если числа равны, иначе False. При несовместимых типах возвращает NotImplemented.
         """
         if isinstance(other, Rational):
             return self.__numerator == other.__numerator and self.__denominator == other.__denominator
@@ -143,19 +156,23 @@ class Rational:
 
     def __float__(self):
         """
-        Преобразует рациональное число в float.
+        Преобразует рациональное число в число с плавающей точкой.
+        :return: Значение рационального числа как float.
         """
         return self.__numerator / self.__denominator
 
     def __abs__(self):
         """
-        Возвращает модуль рационального числа.
+        Возвращает модуль (абсолютное значение) рационального числа.
+        :return: Новое рациональное число, представляющее модуль текущего числа.
         """
         return Rational(abs(self.__numerator), abs(self.__denominator))
 
     def __str__(self):
         """
-        Возвращает строковое представление дроби.
+        Возвращает строковое представление рационального числа.
+        :return: Строковое представление дроби в формате "числитель/знаменатель".
+                 Если знаменатель равен 1, возвращается только числитель.
         """
         if self.__denominator == 1:
             return str(self.__numerator)
@@ -164,6 +181,7 @@ class Rational:
     def __repr__(self):
         """
         Возвращает формальное строковое представление объекта Rational.
+        :return: Формальное представление объекта в формате "Rational(числитель, знаменатель)".
         """
         return f"Rational({self.__numerator}, {self.__denominator})"
 
